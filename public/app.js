2dec1e1e1e9da60d500a2108270f38ed67e05a43
Jesus Alcaraz <jesusalc@gmail.com>
2016-12-09 13:00:09 +0100

    added css classes

--- public/app.js
+++ public/app.js
@@ -58,9 +58,6 @@ function createXMLHTTPObject() {
 }
 
 function syntaxHighlight(json) {
-  if (typeof json != 'string') {
-    json = JSON.stringify(json, undefined, 2);
-  }
   json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
   return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
     var cls = 'number';
@@ -77,66 +74,6 @@ function syntaxHighlight(json) {
     }
     return '<span class="' + cls + '">' + match + '</span>';
   });
-}
-
-
-function syntaxHighlight(json) {
-  json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-  return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-    var cls = 'number';
-    if (/^"/.test(match)) {
-      if (/:$/.test(match)) {
-        cls = 'key';
-      } else {
-        cls = 'string';
-      }
-    } else if (/true|false/.test(match)) {
-      cls = 'boolean';
-    } else if (/null/.test(match)) {
-      cls = 'null';
-    }
-    return '<span class="' + cls + '">' + match + '</span>';
-  });
-}
-
-function hasClass(ele, cls) {
-  return ele.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
-}
-
-function addClass(ele, cls) {
-  if (!this.hasClass(ele, cls)) ele.className += " " + cls;
-}
-
-function removeClass(ele, cls) {
-  if (hasClass(ele, cls)) {
-    var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
-    ele.className = ele.className.replace(reg, ' ');
-  }
-}
-
-function replaceClass(ele, oldClass, newClass){
-  if(hasClass(ele, oldClass)){
-    removeClass(ele, oldClass);
-    addClass(ele, newClass);
-  }
-  return;
-}
-
-function toggleClass(ele, cls1, cls2){
-  if(hasClass(ele, cls1)){
-    replaceClass(ele, cls1, cls2);
-  }else if(hasClass(ele, cls2)){
-    replaceClass(ele, cls2, cls1);
-  }else{
-    addClass(ele, cls1);
-  }
-}
-
-function dropdown(event, elementName) {
-  var ele = window.document.getElementById(elementName);
-  if (hasClass(ele, 'dropdown')) {
-    toggleClass(ele, 'open');
-  }
 }
 
 function changeFormat(event, formatProvided) {
